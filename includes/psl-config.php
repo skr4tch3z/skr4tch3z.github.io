c7b59eca020cfc6a513ad042c157d13e9538a953
skr4tch3z <skr4tch3z@yahoo.com>
2017-04-09 17:37:24 -0700

    import old current site
    imported website from web

---
+++ includes/psl-config.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * These are the database login details
+ */  
+define("HOST", "localhost");     // The host you want to connect to.
+define("USER", "melodyc_gabe");    // The database username. 
+define("PASSWORD", "10thingsihateaboutyou");    // The database password. 
+define("DATABASE", "melodyc_securelogin");    // The database name.
+ 
+define("CAN_REGISTER", "any");
+define("DEFAULT_ROLE", "member");
+ 
+define("SECURE", FALSE);    // FOR DEVELOPMENT ONLY!!!!
